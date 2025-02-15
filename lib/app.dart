21255f63d3a6ad5f7cdbb41a0f546c8c0341a436
Praphulla06 <praphullapradhananga815@gmail.com>
2025-02-15 12:15:57 +0545

    Auth navigation fix

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:harmony_hub/core/navigation/app_router.dart';
-import 'package:harmony_hub/core/state_management/theme/cubits/theme_cubit.dart';
-import 'package:harmony_hub/core/state_management/theme/states/theme_state.dart';
-import 'package:harmony_hub/features/auth/data/repositories/firebase_app_user_repo_impl.dart';
+import 'package:harmony_hub/core/state_management/theme/cubits/theme_cubit.dart';import 'package:harmony_hub/features/auth/data/repositories/firebase_app_user_repo_impl.dart';
 import 'package:harmony_hub/features/auth/domain/repositories/app_user_repo.dart';
 import 'package:harmony_hub/features/auth/presentation/bloc/cubits/auth_cubit.dart';
-import 'package:harmony_hub/features/auth/presentation/bloc/states/auth_states.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -20,27 +17,10 @@ class MyApp extends StatelessWidget {
         BlocProvider(
             create: (context) => AuthCubit(authRepo: authRepo)..checkAuth()),
       ],
-      child: BlocBuilder<ThemeCubit, ThemeState>(
-        builder: (context, themeState) {
-          return BlocListener<AuthCubit, AuthState>(
-            listener: (context, authState) {
-              if (authState is Authenticated) {
-                Future.microtask(() => goRouter.go('/home'));
-              } else if (authState is Unauthenticated) {
-                Future.microtask(() => goRouter.go('/auth'));
-              } else if (authState is AuthError) {
-                Scaffold(
-                  body: SnackBar(content: Text(authState.message)),
-                );
-              }
-            },
-            child: MaterialApp.router(
-              debugShowCheckedModeBanner: false,
-              theme: context.watch<ThemeCubit>().themeData,
-              routerConfig: goRouter,
-            ),
-          );
-        },
+      child: MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        theme: context.watch<ThemeCubit>().themeData,
+        routerConfig: goRouter,
       ),
     );
   }
--- lib/core/navigation/app_router.dart
+++ lib/core/navigation/app_router.dart
@@ -1,4 +1,7 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:harmony_hub/features/auth/presentation/bloc/cubits/auth_cubit.dart';
+import 'package:harmony_hub/features/auth/presentation/bloc/states/auth_states.dart';
 import 'package:harmony_hub/features/auth/presentation/pages/auth_page.dart';
 import 'package:harmony_hub/features/home/presentation/pages/home_page.dart';
 import 'package:harmony_hub/features/notes/presentation/pages/note_page.dart';
@@ -6,6 +9,18 @@ import 'package:harmony_hub/features/planner/presentation/pages/planner_page.dar
 import 'package:harmony_hub/features/settings/presentation/pages/settings_page.dart';
 
 GoRouter goRouter = GoRouter(
+  redirect: (context, routeState) {
+    final authState = context.read<AuthCubit>().state;
+
+    final bool isAuthenticated = authState is Authenticated;
+    final bool isGoingToLogin = routeState.matchedLocation == '/auth';
+
+    if (!isAuthenticated && !isGoingToLogin) {
+      return '/auth';
+    }
+    return null;
+  },
+  initialLocation: '/home',
   routes: [
     GoRoute(path: '/home', builder: (context, state) => HomePage()),
     GoRoute(path: '/notes', builder: (context, state) => NotePage()),
